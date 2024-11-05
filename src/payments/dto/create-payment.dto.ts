6a983d6220cf3142034ab50da2d946d1457c539f
Alejandro Larreta Vzla <larreta_vzla@hotmail.com>
2024-11-04 22:12:34 -0700

    Dto de payments

--- src/payments/dto/create-payment.dto.ts
+++ src/payments/dto/create-payment.dto.ts
@@ -1,1 +1,18 @@
-export class CreatePaymentDto {}
+import { Type } from "class-transformer";
+import { IsBoolean, IsDate, IsDecimal, IsOptional } from "class-validator";
+
+export class CreatePaymentDto {
+  @IsDecimal({ decimal_digits: '2', force_decimal: true })
+  paymentAmount: number;
+
+  @Type(() => Date)
+  @IsDate()
+  paymentDate: Date;
+
+  @IsBoolean()
+  isLate: boolean;
+
+  @IsDecimal({ decimal_digits: '2', force_decimal: true })
+  @IsOptional()
+  lateFees?: number;
+}
